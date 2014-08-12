ce390dfdecd576da5249a17ec0414a3fc5df4d01
Marios <mariosbaz@gmail.com>
2014-08-13 00:15:34 +0300

    Initial commit devise users no users controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Railsforum::Application.config.secret_key_base = '9f2cc8622e4d18e686bfd56a650c5cc5057287a4fb651fbdf6a0d168705a241cdac056ede9d60b835685a3e169b02ff11557fbe8f9be13ad5c654e634143f62b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Railsforum::Application.config.session_store :cookie_store, key: '_railsforum_session'
