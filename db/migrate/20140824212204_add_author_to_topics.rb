44b3a3e6d405f350a81fea39d19931e11431f6d0
Marios <mariosbaz@gmail.com>
2014-09-18 18:11:46 +0300

    finished-voting-system

--- db/migrate/20140824212204_add_author_to_topics.rb
+++
@@ -1,5 +0,0 @@
-class AddAuthorToTopics < ActiveRecord::Migration
-  def change
-    add_column :topics, :topic_author, :string
-  end
-end
--- db/migrate/20140824212501_add_author_to_posts.rb
+++
@@ -1,5 +0,0 @@
-class AddAuthorToPosts < ActiveRecord::Migration
-  def change
-    add_column :posts, :post_author, :string
-  end
-end
