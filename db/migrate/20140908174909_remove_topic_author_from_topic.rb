2c8247813d1447b5e46a570735c91a9ef5922a9b
Marios <mariosbaz@gmail.com>
2014-09-19 05:25:25 +0300

    finished-voting-system

--- db/migrate/20140908174909_remove_topic_author_from_topic.rb
+++
@@ -1,5 +0,0 @@
-class RemoveTopicAuthorFromTopic < ActiveRecord::Migration
-  def change
-    remove_column :topics, :topic_author, :string
-  end
-end
--- db/migrate/20140908175011_remove_post_author_from_post.rb
+++
@@ -1,5 +0,0 @@
-class RemovePostAuthorFromPost < ActiveRecord::Migration
-  def change
-    remove_column :posts, :post_author, :string
-  end
-end
