808e8d3a2c071c87629fff5205a8a87bb3855d7c
Marios <mariosbaz@gmail.com>
2014-10-06 06:30:45 +0300

    fixed-design

--- db/migrate/20141006002839_change_default_values_user.rb
+++ db/migrate/20141006002839_change_default_values_user.rb
@@ -1,9 +1,6 @@
 class ChangeDefaultValuesUser < ActiveRecord::Migration
   def change
-     remove_column :users, :gender
-     remove_column :users, :birthday
      add_column :users, :gender, :string, default: "unknown"
      add_column :users, :birthday, :string, default: "unknown"
-
   end
 end
